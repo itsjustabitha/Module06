5b8a1497cf00bb2166e3221a43711a338691fd57
Tabitha Miller <155591728+itsjustabitha@users.noreply.github.com>
2026-04-02 20:05:12 -0500

    loginformslide77

--- first-react-app/src/components/MoviesList.jsx
+++ first-react-app/src/components/MoviesList.jsx
@@ -66,8 +66,42 @@ return (
 )
 }
 
+
+function AddMovieForm({onAddMovie}) {
+const [title, setTitle] = useState('')
+const [year, setYear] = useState('')
+// ++ add support for the synopsis field as well, here and below
+const handleSubmit = (e) => {
+e.preventDefault();
+onAddMovie({title, year})
+}
+return (
+<div className="AddMovieForm componentBox">
+<form onSubmit={handleSubmit}>
+<label>Movie Title:
+<input name="title" value={title}
+onChange={(e) => setTitle(e.target.value)} />
+</label>
+<label>Year Released:
+<input name="year" type="number" value={year}
+onChange={(e) => setYear(e.target.value)} />
+</label>
+<button>Add Movie</button>
+</form>
+</div>
+)
+}
+// add this in MoviesList component
+const handleAddMovie = (newMovie) => {
+newMovie.id = currentMovies.length + 1; // unreliable but
+succinct
+setCurrentMovies([...currentMovies, newMovie])
+}
+
+
 return (
 <div className="MoviesList FancyBox">
+    <AddMovieForm onAddMovie={handleAddMovie}/>
 <ul> 
 
 {/* { movies.map(movie => (
@@ -79,6 +113,7 @@ return (
 </ul>
 <button onClick={handleReverseMovies}>Reverse List</button>
 </div>
+
 )
 }
 
